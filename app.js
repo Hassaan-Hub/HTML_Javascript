7f61f2a544fe55c6ccff357387818de162eabb9f
hassaan-hub <ophassaan451@gmail.com>
2025-05-25 23:32:16 +0500

    app.js part 1

---
+++ app.js
@@ -0,0 +1,42 @@
+//alert("Hello World");
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//alert(c)
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log(c);
+
+// var x = 5;
+// var y = "ali";
+// var z = y+x;
+// alert(z);
+
+// var x = "hassan";
+// var y = "ali";
+// var z = y+x;
+// alert(z);
+
+// var f = 20;
+// var g = "10";
+// var h = f+g;
+// alert(h)
+
+// var a =  3;
+// var b = 5;
+// var c = 7; 
+// var d = (b - a) * c;
+// alert(d)
+
+// var a = 5; 
+// var b = ++a;
+// alert(b);
+
+var x = 10;
+var y = x++ + ++x - x + x++ - ++x + x;
+        //10  + 12  - 12 + 12 - 14  + 14
+//22
+alert(y)
