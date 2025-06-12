e9a2122496419940bc4bedc2673da6f8ca825d61
hassaan-hub <hassaanali994@gmail.com>
2025-06-13 01:24:47 +0500

    updated

--- 1st_class/2nd_class/3rd_class/4rd_class/5th_class/6th_class/7th_class/8th_ class/class_9th/app.js
+++ 1st_class/2nd_class/3rd_class/4rd_class/5th_class/6th_class/7th_class/8th_ class/class_9th/app.js
@@ -1,3 +1,83 @@
-for(var a = 0; a<10 ; a++){
-    console.log(a);
+// for(var a = 0; a<10 ; a++){
+//     console.log(a);
+// }
+
+
+// var b = 0;
+// while(b<10){
+//     console.log(b);
+//     b++;
+// }
+
+
+
+// var c = 0;
+// do{
+//     console.log(c);
+//     c++;
+// }
+// while(c<10);
+
+
+
+// function foo(){
+//     var a = 213+42998;
+//     alert(a)
+// }
+
+
+
+
+// function foo(greet){
+//     alert(greet)
+// }
+
+
+
+// function hay(){
+//     alert("Click")
+// }
+
+
+
+// function getName(){
+//     var name = document.getElementById("name");
+//     alert(name.value)
+// }
+
+
+
+
+// function setName() {
+//     var name = document.getElementById('xyz');
+//     name.value = "hassaan";
+
+//     var para = document.getElementById("para");
+//     para.innerHTML = "Hello this is example";
+// }
+// window.onload = function() {
+//     setName();
+// }
+
+
+
+function getNumber(num) {
+    var result = document.getElementById("result");
+    result.value += num;
 }
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = ""
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+    
+}
+
+
+
+
+
